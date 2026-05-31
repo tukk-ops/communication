f786b0a730835a37155884040a190e2ea39ad8df
ccwwee <guancongdu@gmail.com>
2026-05-31 16:31:48 +0800

    Delete 學校作業 directory

--- 學校作業/pd.py
+++
@@ -1,9 +0,0 @@
-def Payment(money,pp):
-    money= int(money*1.1)
-    one = int(money//pp)
-    print(f"Total: NT${money}, Per person: NT${one}")
-
-data = input().split()
-pp = int(data[0])
-money = int(data[1])
-Payment(money,pp)
