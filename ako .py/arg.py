fd86c35d4fe40ae6d70fa8541a243b91d0354ad4
ccwwee <guancongdu@gmail.com>
2026-05-31 16:32:19 +0800

    Delete ako .py directory

--- ako .py/arg.py
+++
@@ -1,8 +0,0 @@
-#args 任意數量的參數
-def add(*args):
-    total =0
-    for arg in args:
-        print(f"arg:{arg}")
-        total += arg
-    return total
-print(add(1,2,2))
--- ako .py/module2.py
+++
@@ -1,8 +0,0 @@
-#print("模組2__name__:"+ __name__ )
-import module
-
-if __name__ == "__main__":
-    print("模組2__name__==__main__>")
-
-def hello():
-    print("hi")
